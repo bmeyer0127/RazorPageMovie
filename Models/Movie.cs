ada545c2f4e3940eaaebd526aa75ff2e81c9fe22
Brett Meyer <bmeyer0127@gmail.com>
2024-03-02 13:47:27 -0500

    Updated Models/Movies.cs to make displayed terms more readable and currency match that of the database. Updated page directives for Edit, Details and Delete to be dependent on an int at the end of the URL.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,13 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models;
 
 public class Movie
 {
   public int Id { get; set; }
-  public string? Title { get; set; }
-  [DataType(DataType.Date)]
+  public string Title { get; set; } = string.Empty;
+  [Display(Name = "Release Date")]        //Adds the space between the words in what is displayed
+  [DataType(DataType.Date)]               //Removes the time, keeps the date
   public DateTime ReleaseDate { get; set; }
   public string? Genre { get; set; }
+  [Column(TypeName = "decimal(18, 2)")]   //Maps Price to the currency in the database
   public decimal Price { get; set; }
 }
