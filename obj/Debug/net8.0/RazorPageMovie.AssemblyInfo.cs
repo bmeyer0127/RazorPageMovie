8c07f002c9dab7085ee0662b33775022c639c4b2
Brett Meyer <bmeyer0127@gmail.com>
2024-03-14 14:04:18 -0400

    Syncing PC with github

--- obj/Debug/net8.0/RazorPageMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/RazorPageMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPageMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad3d4994bff691bd56a9fbac12544a9d8117de9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPageMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPageMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
